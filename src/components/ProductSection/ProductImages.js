db82f8d8491d5d24ba7111f667baf0752efc1cb6
Caio <whoiscaio@protonmail.com>
2021-12-31 09:13:18 -0300

    product images section done

--- src/components/ProductSection/ProductImages.js
+++ src/components/ProductSection/ProductImages.js
@@ -1,9 +1,32 @@
 import { ProductImagesContainer } from './styles';
 
+import mainImage from '../../assets/images/image-product-1.jpg';
+
+import thumbnail1 from '../../assets/images/image-product-1-thumbnail.jpg';
+import thumbnail2 from '../../assets/images/image-product-2-thumbnail.jpg';
+import thumbnail3 from '../../assets/images/image-product-3-thumbnail.jpg';
+import thumbnail4 from '../../assets/images/image-product-4-thumbnail.jpg';
+
 function ProductImages() {
   return (
     <ProductImagesContainer>
-      ProductImagesContainer
+      <div className="main-image">
+        <img src={mainImage} alt="Selected" />
+      </div>
+      <div className="thumbnails">
+        <div className="select">
+          <img src={thumbnail1} alt="Thumbnail 1" />
+        </div>
+        <div>
+          <img src={thumbnail2} alt="Thumbnail 2" />
+        </div>
+        <div>
+          <img src={thumbnail3} alt="Thumbnail 3" />
+        </div>
+        <div>
+          <img src={thumbnail4} alt="Thumbnail 4" />
+        </div>
+      </div>
     </ProductImagesContainer>
   )
 }
--- src/components/ProductSection/styles.js
+++ src/components/ProductSection/styles.js
@@ -2,9 +2,33 @@ import styled from 'styled-components';
 
 export const ProductInfoContainer = styled.div``;
 
-export const ProductImagesContainer = styled.div``;
+export const ProductImagesContainer = styled.div`
+  max-width: 580px;
+  padding: 4rem 8rem;
+
+  img {
+    width: 100%;
+    border-radius: 1.6rem;
+  }
+
+  .main-image {
+    margin-bottom: 2rem;
+  }
+
+  .thumbnails {
+    display: flex;
+    justify-content: space-between;
+
+    div {
+      width: 20%;
+    }
+  }
+`;
 
 export default styled.div`
-  max-width: 1400px;
-  margin: 6rem auto 0;
+  max-width: 1200px;
+  margin: 0 auto 0;
+
+  display: flex;
+  align-items: center;
 `;
