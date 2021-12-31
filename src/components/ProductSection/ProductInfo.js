22475c4e4c643dff287215c5d0a635611d90764b
Caio <whoiscaio@protonmail.com>
2021-12-31 11:35:37 -0300

    product info section done

--- src/components/ProductSection/ProductInfo.js
+++ src/components/ProductSection/ProductInfo.js
@@ -1,11 +1,44 @@
 import { ProductInfoContainer } from './styles';
 
+import whiteCartIcon from '../../assets/images/icon-white-cart.png';
+
 function ProductInfo() {
   return (
     <ProductInfoContainer>
-      ProductInfoContainer
+      <header>
+        <span className="company">SNEAKER COMPANY</span>
+        <h1>Fall Limited Edition Sneakers</h1>
+      </header>
+      <section className="description">
+        <p>
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable rubber outer sole, they’ll withstand everything
+          the weather can offer.
+        </p>
+      </section>
+      <section className="price">
+        <span className="main-price">
+          <span>$125.00</span>
+        </span>
+        <div className="discount">
+          <span>50%</span>
+        </div>
+        <div className="old-price">
+          <span>$250.00</span>
+        </div>
+      </section>
+      <section className="actions">
+        <div className="set-quantity">
+          <button type="button">-</button>
+          <span>0</span>
+          <button type="button">+</button>
+        </div>
+        <div className="add-to-cart">
+          <button type="button"><img src={whiteCartIcon} alt="Cart Icon" /><span>Add to cart</span></button>
+        </div>
+      </section>
     </ProductInfoContainer>
-  )
+  );
 }
 
 export default ProductInfo;
--- src/components/ProductSection/styles.js
+++ src/components/ProductSection/styles.js
@@ -1,6 +1,132 @@
 import styled from 'styled-components';
 
-export const ProductInfoContainer = styled.div``;
+export const ProductInfoContainer = styled.div`
+  header {
+    margin-bottom: 4rem;
+
+    .company {
+      color: hsl(26,100%,55%);
+      font-weight: 700;
+      font-size: 1.4rem;
+      letter-spacing: .2rem;
+    }
+
+    h1 {
+      font-size: 3.6rem;
+      margin-top: 2rem;
+    }
+  }
+
+  .description {
+    color: hsl(219,9%,45%);
+    font-size: 1.5rem;
+    line-height: 2.6rem;
+  }
+
+  .price {
+    position: relative;
+    margin: 2rem 0;
+
+    .main-price {
+      font-weight: 700;
+      font-size: 2.8rem;
+    }
+
+    .old-price {
+      color: hsl(220, 14%, 75%);
+      text-decoration: line-through;
+      font-size: 1.6rem;
+      font-weight: 700;
+    }
+
+    .discount {
+      position: absolute;
+      top: .4rem;
+      left: 11rem;
+
+      padding: .4rem;
+
+      border-radius: .4rem;
+      background: hsl(25, 100%, 94%);
+      color: hsl(26,100%,55%);
+
+      font-size: 1.6rem;
+      font-weight: 700;
+    }
+  }
+
+  .actions {
+    display: flex;
+
+    .set-quantity {
+      display: flex;
+      align-items: center;
+      
+      background: hsl(223, 64%, 98%);
+      border-radius: 1rem;
+
+      button {
+        color: hsl(26,100%,55%);
+        font-weight: 700;
+        font-size: 2.8rem;
+        padding: .8rem 1.6rem;
+
+        background: none;
+        border: 0;
+
+        cursor: pointer;
+        transform: translateY(-8%);
+        transition: transform .3s ease-out;
+        
+        &:hover {
+          transform: translateY(-8%) scale(1.2);
+        }
+      }
+
+      span {
+        font-size: 1.6rem;
+        font-weight: 700;
+        padding: .8rem 1.6rem;
+      }
+    }
+
+    .add-to-cart {
+      flex: 1;
+      padding-left: 3rem;
+      
+      button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        width: 100%;
+        height: 100%;
+
+        background: hsl(26,100%,55%);
+        border: 0;
+        border-radius: 1rem;
+        color: #fff;
+
+        font-size: 1.6rem;
+        font-weight: 700;
+
+        padding: .4rem 0;
+
+        cursor: pointer;
+        transition: opacity .3s ease-out;
+
+        &:hover {
+          opacity: .6;
+        }
+
+        img {
+          width: 2rem;
+          margin-right: 1.5rem;
+        }
+      }
+    }
+  }
+`;
 
 export const ProductImagesContainer = styled.div`
   max-width: 580px;
